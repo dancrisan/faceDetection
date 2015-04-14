2cd3772585be6dbb54c848eb7c23eca21b4a80af
dancrisan <dan.crisan@mail.mcgill.ca>
2015-04-14 14:01:32 -0400

    clean code of face detection from picture

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # faceDetection
 Fun with OpenCV and C++ : an algo that detects where are the faces on a picture
+
+Open cmd line where your faceDetection.cpp folder is and write:
+
+1) g++ -ggdb `pkg-config --cflags opencv` -o `basename faceDetection.cpp .cpp` faceDetection.cpp `pkg-config --libs opencv`
+
+2) ./faceDetection
--- faceDetection.cpp
+++ faceDetection.cpp
@@ -8,100 +8,38 @@
 using namespace std;
 using namespace cv;
 
-// Function Headers
 vector < vector<Point> > detectAndDisplay(Mat frame);
 
-// Global variables
-// Copy this file from opencv/data/haarscascades to target folder
+// to find where your module is, type "locate haarcascade_frontalface_alt.xml" in your cmd line
 string face_cascade_name = "/usr/share/opencv/haarcascades/haarcascade_frontalface_alt.xml";
 CascadeClassifier face_cascade;
-string window_name = "Capture - Face detection";
-string filename;
 
-// Function main
 int main(void)
 {
   vector < vector<Point> > faceDetectionVector;
 
+  // Load the cascade
+  if (!face_cascade.load(face_cascade_name))
+  {
+      printf("Couldn't load face cascade module\n");
+      return (-1);
+  };
 
-    // Load the cascade
-    if (!face_cascade.load(face_cascade_name))
-    {
-        printf("Couldn't load face cascade module\n");
-        return (-1);
-    };
+  // Read the image file
+  Mat frame = imread("lenna.png");
 
-    // Read the image file
-    Mat frame = imread("group.JPG");
-    //Mat frame = imread("lenna.png");
+  if (!frame.empty())
+  {
+      faceDetectionVector = detectAndDisplay(frame); //the magic happens here : detects faces from a pic
+      waitKey(0);
+  }
+  else
+      printf("Are you sure you have the correct filename?\n");
 
-
-    //for (;;)
-    //{
-        // Apply the classifier to the frame
-        if (!frame.empty())
-        {
-            faceDetectionVector = detectAndDisplay(frame);
-            waitKey(0);
-            //break;
-        }
-        else
-        {
-            printf("Are you sure you have the correct filename?\n");
-            //break;
-        }
-
-        // int c = waitKey(10);
-        //
-        // if (27 == char(c))
-        // {
-        //     break;
-        // }
-    //}
-    cout << faceDetectionVector.size() << endl;
-    return 0;
+  return 0;
 }
 
-// //face detector
-// int faceDetector()
-// {
-//     // Load the cascade
-//     if (!face_cascade.load(face_cascade_name))
-//     {
-//         printf("Couldn't load face cascade module\n");
-//         return (-1);
-//     };
-//
-//     // Read the image file
-//     Mat frame = imread(filename);
-//
-//     for (;;)
-//     {
-//         // Apply the classifier to the frame
-//         if (!frame.empty())
-//         {
-//             detectAndDisplay(frame);
-//         }
-//         else
-//         {
-//             printf("Did you mispelled your filename?");
-//             break;
-//         }
-//
-//         int c = waitKey(10);
-//
-//         if (27 == char(c))
-//         {
-//             break;
-//         }
-//     }
-//
-//     return 0;
-// }
-
-
-
-// Function detectAndDisplay
+// detecs faces and displays them in the picture
 vector < vector<Point> > detectAndDisplay(Mat frame)
 {
     std::vector<Rect> faces;
@@ -109,12 +47,8 @@ vector < vector<Point> > detectAndDisplay(Mat frame)
     Mat crop;
     Mat res;
     Mat gray;
-    string text;
-    stringstream sstm;
     vector<Point> faceCoords;
     vector < vector<Point> > faceVector;
-
-    //vector <vector <int x, int y> > coords;
 
     cvtColor(frame, frame_gray, COLOR_BGR2GRAY);
     equalizeHist(frame_gray, frame_gray);
@@ -122,74 +56,21 @@ vector < vector<Point> > detectAndDisplay(Mat frame)
     // Detect faces
     face_cascade.detectMultiScale(frame_gray, faces, 1.1, 2, 0 | CASCADE_SCALE_IMAGE, Size(30, 30));
 
-    // Set Region of Interest
-    cv::Rect roi_b;
-    cv::Rect roi_c;
+    int i;
 
-    size_t ic = 0; // ic is index of current element
-    int ac = 0; // ac is area of current element
-
-    size_t ib = 0; // ib is index of biggest element
-    int ab = 0; // ab is area of biggest element
-
-    for (ic = 0; ic < faces.size(); ic++) // Iterate through all current elements (detected faces)
-
+    for (i = 0; i < faces.size(); i++) // Iterate through all detected faces
     {
-        roi_c.x = faces[ic].x;
-        roi_c.y = faces[ic].y;
-        roi_c.width = (faces[ic].width);
-        roi_c.height = (faces[ic].height);
-
-        ac = roi_c.width * roi_c.height; // Get the area of current element (detected face)
-
-        roi_b.x = faces[ib].x;
-        roi_b.y = faces[ib].y;
-        roi_b.width = (faces[ib].width);
-        roi_b.height = (faces[ib].height);
-
-        ab = roi_b.width * roi_b.height; // Get the area of biggest element, at beginning it is same as "current" element
-
-        if (ac > ab)
-        {
-            ib = ic;
-            roi_b.x = faces[ib].x;
-            roi_b.y = faces[ib].y;
-            roi_b.width = (faces[ib].width);
-            roi_b.height = (faces[ib].height);
-        }
-
-        crop = frame(roi_b);
-        resize(crop, res, Size(128, 128), 0, 0, INTER_LINEAR); // This will be needed later while saving images
-        cvtColor(crop, gray, CV_BGR2GRAY); // Convert cropped image to Grayscale
-
-        Point pt1(faces[ic].x, faces[ic].y); // Display detected faces on main window - live stream from camera
+        Point pt1(faces[i].x, faces[i].y); // Display detected faces on main window
         faceCoords.push_back(pt1);
-        Point pt2((faces[ic].x + faces[ic].height), (faces[ic].y + faces[ic].width));
+        Point pt2((faces[i].x + faces[i].height), (faces[i].y + faces[i].width));
         faceCoords.push_back(pt2);
         rectangle(frame, pt1, pt2, Scalar(0, 255, 0), 2, 8, 0);
-        //cout << faceCoords.size() << endl;
-        //cout << faceCoords[0] << endl;
-        //cout << faceCoords[1] << endl;
+
         faceVector.push_back(faceCoords);
         faceCoords.clear();
-        //cout << faceCoords.size() << endl;
-        //cout << faceVector.size() << endl;
-        //cout << faceVector[0] << endl;
-        //cout << faceVector[1] << endl;
-        //cout << faces[ic].x << endl;
-        //printf("Coord 2 is " + faces[ic].y);
-        //coords.push(vector <pt1, pt2>);
-        //coords.size();
     }
 
-    // Show image
-    //sstm << "Crop area size: " << roi_b.width << "x" << roi_b.height << " Filename: " << filename;
-    //text = sstm.str();
-
-    //putText(frame, text, cvPoint(30, 30), FONT_HERSHEY_COMPLEX_SMALL, 0.8, cvScalar(0, 0, 255), 1, CV_AA);
     imshow("original", frame);
 
     return faceVector;
-    //return coords;
-
 }
